393d4fb31bc58ab548291a25c8296c24ff0b29e6
Bálint Kozák <kozak.balint15@gmail.com>
2024-09-27 16:19:32 +0200

    removed unused functions from errors.go

--- backend/cmd/api/errors.go
+++ backend/cmd/api/errors.go
@@ -44,12 +44,3 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
-
-func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
-	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
-}
-
-func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
-	message := "unable to update the record due to an edit conflict, please try again"
-	app.errorResponse(w, r, http.StatusConflict, message)
-}
